3a7eda079574431996a09eb1540d8db57ad0a67c
Keith Mannock <k.mannock@bbk.ac.uk>
2023-05-24 11:18:22 +0100

    sync

---
+++ topic-01/flask-example/README.md
@@ -0,0 +1,68 @@
+# Flask Example
+Creating a simple Flask Example project
+
+Part of this code and explination is modifyfied from https://flask.palletsprojects.com/en/2.3.x/
+
+## Codio notes
+This project was created to help student on AES. Parts 1 to 8 have been corrected to align with codio expected file layout
+
+## Part 9
+In this Part
+-
+
+Link to [part 9 branch](https://github.com/stealthness/flask-example/tree/part9-adding-form-application)
+
+
+
+## Part 8
+In this part
+- a page to all users using a table, with added css to style the table
+- made modification to header, created its own html file
+
+Link to [part 8 branch](https://github.com/stealthness/flask-example/tree/part8-adding-allusers-application)
+
+## Part 7
+In this part
+- added intial user list store on a 'user_data.csv'
+- added simple unit test for 'data_manager' 
+ 
+Link to [part 7 branch](https://github.com/stealthness/flask-example/tree/part7-adding-csv-application)
+
+## Part 6
+In this part 
+- added stylesheet via a static file
+- change about to display three random user pages
+- added `base.html` template
+ 
+Link to [part 6 branch](https://github.com/stealthness/flask-example/tree/part6-using-base-template-application)
+
+## Part 5
+In this part we have
+- added DataManager class to handle a list of users
+- modified user page to check if user in list of user or redirect to error page
+
+Link to [part 5 branch](https://github.com/stealthness/flask-example/tree/part5-basic-template-application)
+
+## Part 4
+In this part we have;
+- Adding simple user page
+- Used `{% include <some.html' %}` to replace footer navigation bar with single html file
+
+Link to [part 4 branch](https://github.com/stealthness/flask-example/tree/part4-basic-template-application)
+
+## Part 3
+In this part we have;
+- Adding an error page
+- Used templates 
+
+Link to [part 3 branch](https://github.com/stealthness/flask-example/tree/part3-basic-template-application)
+
+## Part 2
+Adding an about page, and some html tags 
+
+Link to [part 2 branch](https://github.com/stealthness/flask-example/tree/part2-minimal-application)
+
+## Part 1
+Creating the minimal Flask web-application 
+
+Link to [part 1 branch](https://github.com/stealthness/flask-example/tree/part1-minimal-application)
---
+++ topic-01/flask-example/main_app.py
@@ -0,0 +1,76 @@
+import os
+from random import shuffle
+
+from flask import Flask, render_template, redirect, url_for, request
+from markupsafe import escape
+from data_manager import SimpleDataManager
+
+db_filename = os.path.join('res', 'user_db.db')
+csv_users_filename = os.path.join('res', 'full_user_data.csv')
+
+app = Flask(__name__)
+data = SimpleDataManager()
+# data.load_users(os.path.join('res', 'user_data.csv'))
+data.load_user_from_db(csv_users_filename, db_filename)
+
+@app.route('/', methods=(['POST', 'GET']))
+@app.route('/index', methods=(['POST', 'GET']))
+def get_home_page():
+    if request.method == 'POST':
+        user = escape(request.form['name'])
+        if user.title() in data.users:
+            return redirect(url_for('get_user_page', username=user.lower()))
+        else:
+            return redirect(url_for('get_new_user_page'))
+    return render_template('index.html', title='Home Page')
+
+@app.route('/about')
+def get_about_page():
+    users = []
+    for _ in range(3):
+        shuffle(data.users)
+        users = data.users[:3]
+
+    return render_template('about.html', title='Home Page', users=users)
+
+
+@app.route('/warn')
+def get_warning_page(message=''):
+    return render_template('error_page.html', title='Error Page', message=message)
+
+
+@app.route('/user/<string:username>')
+def get_user_page(username):
+    safe_username = escape(username).strip()
+    if safe_username == 'all_users':
+        return redirect(url_for("get_all_users_page"))
+    if not safe_username.title() in data.users:
+        return redirect(url_for('get_warning_page'))
+
+    return render_template('user_page.html', title='User Page' ,username=safe_username)
+
+
+@app.route('/all_users')
+def get_all_users_page():
+    return render_template("all_users.html", title="All Users Page", users=data.users)
+
+
+@app.route('/new_user', methods = ([ 'POST','GET']))
+def get_new_user_page():
+    if request.method == 'POST':
+        new_user = escape(request.form['name']).title()
+        new_location = escape(request.form['location']).title()
+        new_club = escape(request.form['club']).title()
+        if new_user in data.users or new_user == '':
+            return render_template('add_new_user_page.html')
+        data.users.append(new_user)
+        conn = data.get_db_connection(db_filename)
+        conn.execute(f'insert into users values("{new_user}", "{new_location}", "{new_club}");')
+        return redirect(url_for ('get_user_page', username=new_user.lower()))
+    return render_template('add_new_user_page.html', title="New User Page")
+
+if __name__ == "__main__":
+    # app.run()
+
+    # Optional, host address often required if running on a VM
+    app.run(debug=True, host="0.0.0.0")
