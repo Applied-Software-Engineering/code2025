9b738f71ae8a96d8af98c63af9e67cc8e23164c8
Keith Mannock <k.mannock@bbk.ac.uk>
2023-05-30 20:19:31 +0100

    topic-05 slightly functional sqlalchemy example revised version reducing imports

--- topic-05/sqlalchemy/app.py
+++ topic-05/sqlalchemy/app.py
@@ -1,14 +1,11 @@
 import sqlalchemy as db
 from sqlalchemy.orm import Session
 from sqlalchemy import text, select
-from sqlalchemy import MetaData
-from sqlalchemy import Table
 
 if __name__ == "__main__":
     engine = db.create_engine("sqlite:///european_database.sqlite", echo=True)
     session = Session(engine)
     stmt = select(text("name")).select_from(text("divisions"))
-    print(stmt)
     result = session.execute(stmt)
     for divs in result.all():
         print(divs)
