28fdbedb87619582e1fbc90391ec837b2f3ac5d1
Keith Mannock <k.mannock@bbk.ac.uk>
2023-06-04 11:30:47 +0100

    Added ignore statements to plot_iris_dataset

--- topic-05/img_to_csv/convert.py
+++ topic-05/img_to_csv/convert.py
@@ -4,15 +4,15 @@ from PIL import Image
 # read image
 im = Image.open("test.png")
 
-# convert to numpy array
-arr = np.array(im)
+# convert to numpy array using type annotations to specify dtype
+arr = np.array(im)  # type: np.ndarray
 # output shape via print
-print(arr.shape)
+# print(arr.shape)
 
 # convert 3d array to 2d list of lists
 arr = arr.reshape(arr.shape[0], -1).tolist()
 # output shape via print
-print(len(arr), len(arr[0]))
+# print(len(arr), len(arr[0]))
 
 # save list of lists to csv
 np.savetxt("test.csv", arr, delimiter=",")
